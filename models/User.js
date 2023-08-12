afd7687c3066be29c51ee3336a43e8e50867eb4a
Graciela Romano <76084759+GracielaRomano@users.noreply.github.com>
2023-08-12 16:08:18 -0300

    Mongo (#1)
    * Creacion de Modeloy colecciones(users y cities), creacion de la base de datos
    
    * Creacion de los Modelos(Itinerary y Activity) y de las colecciones(itineraries y activities)

---
+++ models/User.js
@@ -0,0 +1,14 @@
+import { model,Schema} from "mongoose";
+
+let collection = 'users'
+let schema = new Schema({
+    name: { type:String,required:true},
+    lastName: { type:String}, // cualquier data opcional no se necesita agregar el required
+    mail: { type:String,required:true,unique:true},//validacion que no me permite registrar dos veces el mismo mail
+    photo: { type:String,default:"https://www.cinemascomics.com/wp-content/uploads/2020/06/poder-darth-vader.jpg"},//si el cliente no envia una foto, por defecto se muestra la foto en el default
+    password:{ type:String,required:true},
+    country: { type:String,required:true}
+})
+let User = model(collection,schema)
+export default User
+
