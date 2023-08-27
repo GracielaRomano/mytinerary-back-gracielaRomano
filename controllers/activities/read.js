fc75656f51b1cb73b498c149416525ee63d08997
Graciela Romano <76084759+GracielaRomano@users.noreply.github.com>
2023-08-27 20:04:04 -0300

    Creacion del crud de activities (#4)

---
+++ controllers/activities/read.js
@@ -0,0 +1,31 @@
+import Activity from "../../models/Activity.js";
+
+export default async (req,res,next) => {
+    try {
+        let queries = {}
+        if(req.query.itinerary_id){
+            queries.itinerary_id = req.query.name
+        }
+        let allActivities = await Activity
+            .find(queries,'-__v -createdAt -updatedAt')
+            .populate({
+                path: "itinerary_id",
+                select:"name photo city_id",
+                populate:{
+                    path: "city_id",
+                    select: "city photo admin_id",
+                    populate: {
+                        path: "admin_id",
+                        select: "name photo"
+                    }
+                }
+            })
+            return res.status(200).json({
+                success: true,
+                message: 'Activities found',
+                response: allActivities
+            })
+    } catch (err) {
+        next(err)
+    }
+}
